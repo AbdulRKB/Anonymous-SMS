669e748f17c35aab19b0123ccf36008b9855b34c
Titus <82715493+CyberTitus@users.noreply.github.com>
2021-09-12 15:31:33 +0100

    setup

---
+++ setup.sh
@@ -0,0 +1,24 @@
+
+
+install(){
+    loading &
+    PID=$!
+    pkg install git > /dev/null
+    pkg install python > /dev/null
+    git clone https://github.com/CyberTitus/Anonymous-SMS.git > /dev/null
+    cd Anonymous-SMS
+    pip install -r requirements.txt
+    pkill $PID
+}
+
+
+loading(){
+    loads=('.  ' '.. ' '...')
+    while :; do
+        for x in "${loads[@]}"; do
+            echo -ne "\r[+] Installing Packages$x"
+            sleep 0.2
+        done
+    done
+}
+install
