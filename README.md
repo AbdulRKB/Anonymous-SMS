07220b451928d171af0692ae411798486178dce1
CyberTitus <82715493+CyberTitus@users.noreply.github.com>
2022-08-08 18:11:27 +0100

    updated: removed bugs & added info

--- README.md
+++ README.md
@@ -1,6 +1,17 @@
+<div align="center">
+
 # AnonymousSMS
-**Send Messages to your friends, Anonymously.**
+### Send Messages to your friends, Anonymously.
+#####   ⚠ Warning! This tool was created solely for educational purposes; I am not liable for your actions! ⚠
+## Installation
+Run the following commands to use the tool
+```bash
+git clone https://github.com/CyberTitus/Anonymous-SMS
+cd Anonymous-SMS
+pip install -r requirements.txt
+python main.py
+```
 ## License
 *This repository is licensed under the [MIT License](https://github.com/CyberTitus/Anonymous-SMS/blob/main/LICENSE)*
 
-
+</div>
---
+++ main.py
@@ -0,0 +1,35 @@
+import requests, click
+
+class SMS:
+	def __init__(self, number, text):
+		self.number = number
+		self.text = text
+		self.url = "https://textbelt.com/text"
+		self.data = {"phone": number, "message": text, "key": "textbelt"}
+
+	def sendSMS(self):
+		res = requests.post(self.url, data=self.data).json()
+		if res['success']:
+			click.secho("[+] Sent Message Successfully!", fg="green")
+		else:
+			click.secho("[-] Couldn't Send Message!", fg="red")
+			click.secho('[-] '+res['error'], fg="red")
+
+
+def main():
+	try:
+		NUMBER = click.prompt('Enter Number (Include country code, e.g. +92)', type=str)
+		TEXT = str(input("Enter Message: "))
+	except:
+		click.secho("\n[-] Good bye!", fg="red")
+		exit(0)
+	sms = SMS(NUMBER, TEXT)
+	if click.confirm('Do you want to send the message?'):
+		sms.sendSMS()
+	else:
+		click.secho("[-] Good bye!", fg="red")
+		exit(0)
+
+
+if __name__ == '__main__':
+	main()
